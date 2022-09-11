233bb0675d945a763e7c53d376d15b3209885aa6
dqnk <64268180+dqnk@users.noreply.github.com>
2022-09-11 14:05:49 +0200

    adding the table

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import { MyForm } from "./MyForm";
+import BasicTable from "./BasicTable";
+import { generate } from "shortid";
 
 const App = () => {
+  const [rows, setRows] = useState([
+    {
+      id: "1",
+      firstName: "First Name",
+      lastName: "Last Name",
+      email: "First",
+    },
+  ]);
+
   return (
     <div style={{ textAlign: "center" }}>
       <MyForm
-        onSubmit={({ email, firstName, lastName }) => {
-          console.log(email, firstName, lastName);
+        onSubmit={(data) => {
+          setRows((currentRows) => [
+            {
+              id: generate(),
+              ...data,
+            },
+            ...currentRows,
+          ]);
         }}
       />
+      <BasicTable rows={rows} />
     </div>
   );
 };
---
+++ client/src/BasicTable.tsx
@@ -0,0 +1,52 @@
+import * as React from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import { withStyles } from "@material-ui/core";
+
+interface Props {
+  rows: Array<{
+    id: string;
+    firstName: string;
+    lastName: string;
+    email: string;
+  }>;
+}
+
+function BasicTable({ rows }: Props) {
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>id</TableCell>
+            <TableCell>FirstName</TableCell>
+            <TableCell>LastName</TableCell>
+            <TableCell>Email</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow
+              key={row.id}
+              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+            >
+              <TableCell>{row.id}</TableCell>
+              <TableCell>{row.firstName}</TableCell>
+              <TableCell component="th" scope="row">
+                {row.lastName}
+              </TableCell>
+              <TableCell>{row.email}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
+
+export default BasicTable;
