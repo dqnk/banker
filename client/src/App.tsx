b22f389ebfa9f8f2be2847d621b71ac6cbabad72
dqnk <erik.kmetic@gmail.com>
2022-09-14 10:06:17 +0200

    dark mode

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,14 @@ import React, { useState } from "react";
 import { MyForm } from "./MyForm";
 import BasicTable from "./BasicTable";
 import { generate } from "shortid";
+import "./App.css";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
 
 const App = () => {
   const [rows, setRows] = useState([
@@ -14,19 +22,23 @@ const App = () => {
   ]);
 
   return (
-    <div style={{ textAlign: "center" }}>
-      <MyForm
-        onSubmit={(data) => {
-          setRows((currentRows) => [
-            {
-              id: generate(),
-              ...data,
-            },
-            ...currentRows,
-          ]);
-        }}
-      />
-      <BasicTable rows={rows} />
+    <div className="App" style={{ textAlign: "center" }}>
+      <header className="App-header">
+        <ThemeProvider theme={darkTheme}>
+          <MyForm
+            onSubmit={(data) => {
+              setRows((currentRows) => [
+                {
+                  id: generate(),
+                  ...data,
+                },
+                ...currentRows,
+              ]);
+            }}
+          />
+          <BasicTable rows={rows} />
+        </ThemeProvider>
+      </header>
     </div>
   );
 };
