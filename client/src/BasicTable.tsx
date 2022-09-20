7f11d21db3065685848216eb4f9b274500e4886d
dqnk <erik.kmetic@gmail.com>
2022-09-20 12:32:23 +0200

    remove old theming

--- client/src/BasicTable.tsx
+++ client/src/BasicTable.tsx
@@ -6,7 +6,6 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
-import { makeStyles } from "@material-ui/core";
 
 interface Props {
   rows: Array<{
@@ -17,20 +16,13 @@ interface Props {
   }>;
 }
 
-const useStyles = makeStyles({
-  container: {
-    maxWidth: 1000,
-  },
-});
-
 function BasicTable({ rows }: Props) {
   //remove last element, which is just the placeholder for initial state
   // TODO: make this better
   rows = rows.slice(0, -1);
 
-  const classes = useStyles();
   return (
-    <TableContainer className={classes.container} component={Paper}>
+    <TableContainer component={Paper}>
       <Table aria-label="simple table">
         <TableHead>
           <TableRow>
