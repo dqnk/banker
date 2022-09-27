58965c5f38a37241f3dcc022a9fc56bb2f2db1d3
dqnk <erik.kmetic@gmail.com>
2022-09-27 12:29:19 +0200

    chart and font color

--- client/src/MyDoughnut.tsx
+++ client/src/MyDoughnut.tsx
@@ -2,6 +2,8 @@ import { Doughnut } from "react-chartjs-2";
 import * as React from "react";
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
 ChartJS.register(ArcElement, Tooltip, Legend);
+ChartJS.defaults.font.size = 20;
+ChartJS.defaults.color = "#fff";
 
 interface Props {
   rows: Array<{
@@ -11,17 +13,27 @@ interface Props {
     email: string;
   }>;
 }
-const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042"];
+const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042", "#FF2299"];
 
 export default function MyDoughnut(props: Props) {
-  const rows = props.rows.slice(0, -1).reverse();
+  const rows = props.rows
+    .slice(0, -1)
+    .sort((a, b) => (a.expenseAmount < b.expenseAmount ? -1 : 1))
+    .map((obj, index) => ({
+      expenseName: obj.expenseName,
+      expenseAmount: obj.expenseAmount,
+      color: COLORS[index % 5],
+    }))
+    .reverse()
+    .slice(0, 5);
   const data = {
     labels: rows.map((obj) => obj.expenseName),
+
     datasets: [
       {
         label: "Portion of spending",
         data: rows.map((obj) => obj.expenseAmount),
-        backgroundColor: rows.map((obj, index) => COLORS[index % 4]),
+        backgroundColor: rows.map((obj) => obj.color),
       },
     ],
   };
